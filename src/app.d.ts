3c280ffea9645b8929ab3657a3b84c7ccacfae70
Johnny Magrippis <j@magrippis.com>
2023-09-01 17:05:58 +0200

    ⚡️  Integrate Supabase

--- src/app.d.ts
+++ src/app.d.ts
@@ -3,8 +3,13 @@
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
+		interface Locals {
+			supabase: SupabaseClient
+			getSession(): Promise<Session | null>
+		}
+		interface PageData {
+			session: Session | null
+		}
 		// interface Platform {}
 	}
 }
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let data
+	$: redirectTo = `${data.url}/auth/callback`
+</script>
+
+<svelte:head>
+	<title>Login</title>
+</svelte:head>
+
+<div>
+	{redirectTo}
+</div>
