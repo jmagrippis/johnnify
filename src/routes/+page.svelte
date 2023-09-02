b67833e41736bc6fba0fe3faca196e4a6c3a2d6e
Johnny Magrippis <j@magrippis.com>
2023-09-02 17:52:26 +0200

    💄 Text gradient for hero

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,10 @@
 </script>
 
 <main class="flex grow flex-col items-center justify-center">
-	<h1 class="text-6xl font-bold">Johnnify</h1>
-	<p class="text-xl">Level up your coding journey!</p>
+	<div
+		class="flex flex-col items-center justify-center bg-gradient-to-r from-primary-600 via-secondary-600 to-secondary-500 bg-clip-text text-transparent drop-shadow-sm"
+	>
+		<h1 class="text-6xl font-bold">Johnnify</h1>
+		<p class="text-xl">Level up your coding journey!</p>
+	</div>
 </main>
