0cebb019a5e3afef143f907e4918ae0aaf0eba64
Johnny Magrippis <j@magrippis.com>
2023-09-02 18:22:26 +0200

    ✨  add Footer with relevant links

---
+++ src/routes/PulsingHeart.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import HeartIcon from './heart.svg?component'
+</script>
+
+<div>
+	<HeartIcon class="absolute w-6 fill-primary-400 hover:animate-ping" />
+	<HeartIcon class="w-6 fill-primary-500" />
+</div>
