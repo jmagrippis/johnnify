6d833b094b24cedd6041a1256434873d54fc4d83
Johnny Magrippis <j@magrippis.com>
2023-09-06 11:35:34 +0200

    ⬆️  upgrade deps

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,7 +1,7 @@
 import {redirect} from '@sveltejs/kit'
 import type {PageServerLoad} from './$types'
 
-export const load: PageServerLoad = async ({url, locals: {getSession}}) => {
+export const load: PageServerLoad = async ({locals: {getSession}}) => {
 	const session = await getSession()
 
 	// if the user is already logged in return them to their profile page
