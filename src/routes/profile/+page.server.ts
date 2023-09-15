46de1a14ba87ef396d90b081ea72494290d13945
Johnny Magrippis <j@magrippis.com>
2023-09-15 14:14:25 +0200

    ✨  get users to the Stripe Customer Portal

--- src/routes/profile/+page.server.ts
+++ src/routes/profile/+page.server.ts
@@ -1,7 +1,10 @@
 import {redirect} from '@sveltejs/kit'
-import type {PageServerLoad} from './$types'
+import type {Actions, PageServerLoad} from './$types'
+import {stripe} from '$lib/server/stripe'
 
-export const load: PageServerLoad = async ({locals: {getSession}}) => {
+export const load: PageServerLoad = async ({
+	locals: {getSession, supabase},
+}) => {
 	const session = await getSession()
 
 	// if the user is NOT logged in return them to the home page
@@ -9,10 +12,42 @@ export const load: PageServerLoad = async ({locals: {getSession}}) => {
 		throw redirect(303, '/login')
 	}
 
+	// check if the user has a subscription
+
+	const {data: subscription} = await supabase
+		.from('subscriptions')
+		.select('status, cancel_at_period_end, cancel_at')
+		.single()
+
 	return {
+		subscription,
 		meta: {
 			title: 'Profile',
 			description: 'Manage your account and view your profile details.',
 		},
 	}
+}
+
+export const actions: Actions = {
+	customerPortalSession: async ({url, locals: {supabase}}) => {
+		const {data, error} = await supabase
+			.from('customers')
+			.select('stripe_customer_id')
+			.single()
+
+		if (error || !data?.stripe_customer_id) {
+			throw new Error('could not find stripe_customer_id for user')
+		}
+
+		const portalSession = await stripe.billingPortal.sessions.create({
+			customer: data.stripe_customer_id,
+			return_url: url.href,
+		})
+
+		if (!portalSession.url) {
+			throw Error('could not create portal session with Stripe')
+		}
+
+		throw redirect(303, portalSession.url)
+	},
 }
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -3,10 +3,8 @@
 
 	export let data
 
-	$: ({supabase, session} = data)
-
 	async function signOut() {
-		const {error} = await supabase.auth.signOut()
+		const {error} = await data.supabase.auth.signOut()
 
 		if (error) {
 			console.error(error)
@@ -15,13 +13,51 @@
 
 		goto('/')
 	}
+
+	const understatedButtonClasses =
+		'underline decoration-emphasis hover:decoration-emphasis-hover'
 </script>
 
-<main class="container grow px-2">
+<main class="container max-w-prose grow px-2 text-lg">
 	<h1 class="text-4xl">Profile</h1>
-	<p>You are logged in as <strong>{session?.user.email}</strong></p>
-	<button
-		class="underline decoration-emphasis hover:decoration-emphasis-hover"
-		on:click={signOut}>logout</button
-	>
+	<p>You are logged in as <strong>{data.session?.user.email}</strong></p>
+
+	{#if data.subscription?.status === 'active'}
+		<section>
+			<p>Thank you so much for maintaining a membership!</p>
+
+			{#if data.subscription?.cancel_at_period_end && data.subscription?.cancel_at}
+				<form
+					class="my-2 rounded bg-surface-2 p-4 shadow-low"
+					method="POST"
+					action="?/customerPortalSession"
+				>
+					<p>
+						Your subscription is going to be cancelled at <strong
+							>{new Intl.DateTimeFormat('en-US').format(
+								new Date(data.subscription.cancel_at),
+							)}</strong
+						>.
+					</p>
+					<p>
+						We’ll be sad to see you go! You still get all membership benefits
+						until then, and if you wish to renew, <button
+							class={understatedButtonClasses}
+							>you may do so in the customer portal</button
+						>.
+					</p>
+					<p>No pressure 😄 Only support if it’s sustainable 🙌</p>
+				</form>
+			{:else}
+				<form method="POST" action="?/customerPortalSession">
+					<button class={understatedButtonClasses}
+						>Manage your billing details or cancel</button
+					>
+				</form>
+			{/if}
+		</section>
+	{/if}
+	<section>
+		<button class={understatedButtonClasses} on:click={signOut}>logout</button>
+	</section>
 </main>
