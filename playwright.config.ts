8142aed01bcfe9b840e4b10fc9cf4453fb3f7f21
Johnny Magrippis <j@magrippis.com>
2023-10-04 16:01:34 +0200

    ✅  use the build server for e2e

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL: 'http://localhost:5173',
+		baseURL: 'http://localhost:4173',
 
 		launchOptions: {
 			slowMo: parseInt(process.env.SLOW_MO || '0'),
@@ -73,8 +73,8 @@ export default defineConfig({
 
 	/* Run your local dev server before starting the tests */
 	webServer: {
-		command: 'bun run --bun dev',
-		port: 5173,
+		command: 'bun run --bun build && bun run --bun preview',
+		port: 4173,
 		reuseExistingServer: !process.env.CI,
 	},
 })
--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -2,8 +2,8 @@ import {test as setup, expect} from '@playwright/test'
 import {loggedInCredentials} from './credentials'
 
 setup('authenticate as a regular loggedIn user', async ({page}) => {
-	// Perform authentication steps. Replace these actions with your own.
 	await page.goto('/login')
+
 	await page
 		.getByLabel('email')
 		.fill('johnnifytech+playwright-logged-in@gmail.com')
@@ -11,6 +11,7 @@ setup('authenticate as a regular loggedIn user', async ({page}) => {
 		.getByLabel('password')
 		.fill(process.env.PLAYWRIGHT_LOGGED_IN_PASSWORD as string)
 	await page.getByRole('button', {name: 'Login with email'}).click()
+
 	// Wait until the page receives the cookies.
 	// Wait until the page reaches a state where all cookies are set.
 	await expect(
@@ -18,6 +19,5 @@ setup('authenticate as a regular loggedIn user', async ({page}) => {
 	).toBeVisible()
 
 	// End of authentication steps.
-
 	await page.context().storageState({path: loggedInCredentials})
 })
