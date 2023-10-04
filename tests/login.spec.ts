506cf5a72c30ac2eb3c208a1b94f8847004e203f
Johnny Magrippis <j@magrippis.com>
2023-10-04 16:01:34 +0200

    ✅  use the build server for e2e

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,25 +1,6 @@
 import {test, expect} from '@playwright/test'
 import {loggedInCredentials} from './credentials'
 
-test('login page', async ({page}) => {
-	// start at the home page
-	await page.goto('/login')
-
-	await expect(page).toHaveTitle(/Login/)
-	await expect(
-		page.getByRole('heading', {name: 'Login', level: 1}),
-	).toBeVisible()
-
-	await expect(
-		page.getByRole('button', {name: 'Login with Github'}),
-	).toBeVisible()
-
-	// user can see the form to create a new account
-
-	await page.getByRole('button', {name: 'create a new account'}).click()
-	await expect(page.getByRole('button', {name: 'Signup'})).toBeVisible()
-})
-
 test.describe(() => {
 	test.use({storageState: loggedInCredentials})
 
