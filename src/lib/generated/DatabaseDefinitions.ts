cd53741ca5ff3833abe3d83cee135b0f8341d939
Johnny Magrippis <j@magrippis.com>
2023-10-24 12:30:47 +0200

    🏷️  update DB types with new `videos` table

--- src/lib/generated/DatabaseDefinitions.ts
+++ src/lib/generated/DatabaseDefinitions.ts
@@ -230,12 +230,92 @@ export interface Database {
 					},
 				]
 			}
+			videos: {
+				Row: {
+					checksum: string
+					content: string
+					created_at: string
+					front_matter: Json
+					id: string
+					published_at: string
+					slug: string
+					updated_at: string
+				}
+				Insert: {
+					checksum: string
+					content: string
+					created_at?: string
+					front_matter?: Json
+					id?: string
+					published_at?: string
+					slug: string
+					updated_at?: string
+				}
+				Update: {
+					checksum?: string
+					content?: string
+					created_at?: string
+					front_matter?: Json
+					id?: string
+					published_at?: string
+					slug?: string
+					updated_at?: string
+				}
+				Relationships: []
+			}
 		}
 		Views: {
 			[_ in never]: never
 		}
 		Functions: {
-			[_ in never]: never
+			hnswhandler: {
+				Args: {
+					'': unknown
+				}
+				Returns: unknown
+			}
+			ivfflathandler: {
+				Args: {
+					'': unknown
+				}
+				Returns: unknown
+			}
+			vector_avg: {
+				Args: {
+					'': number[]
+				}
+				Returns: string
+			}
+			vector_dims: {
+				Args: {
+					'': string
+				}
+				Returns: number
+			}
+			vector_norm: {
+				Args: {
+					'': string
+				}
+				Returns: number
+			}
+			vector_out: {
+				Args: {
+					'': string
+				}
+				Returns: unknown
+			}
+			vector_send: {
+				Args: {
+					'': string
+				}
+				Returns: string
+			}
+			vector_typmod_in: {
+				Args: {
+					'': unknown[]
+				}
+				Returns: number
+			}
 		}
 		Enums: {
 			pricing_plan_interval: 'day' | 'week' | 'month' | 'year'
