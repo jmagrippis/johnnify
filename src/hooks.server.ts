cbf9044bf5647d1ca8ca5b0324edad8ad757bcca
Johnny Magrippis <j@magrippis.com>
2023-11-12 21:28:36 +0100

    👽️ switch to supabase/ssr

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,15 +1,25 @@
-import {createSupabaseServerClient} from '@supabase/auth-helpers-sveltekit'
 import type {Handle} from '@sveltejs/kit'
+import {createServerClient} from '@supabase/ssr'
 
 import {PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY} from '$env/static/public'
 import type {Database} from '$lib/generated/DatabaseDefinitions'
 
 export const handle: Handle = async ({event, resolve}) => {
-	event.locals.supabase = createSupabaseServerClient<Database>({
-		supabaseUrl: PUBLIC_SUPABASE_URL,
-		supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
-		event,
-	})
+	event.locals.supabase = createServerClient<Database>(
+		PUBLIC_SUPABASE_URL,
+		PUBLIC_SUPABASE_ANON_KEY,
+		{
+			cookies: {
+				get: (key) => event.cookies.get(key),
+				set: (key, value, options) => {
+					event.cookies.set(key, value, options)
+				},
+				remove: (key, options) => {
+					event.cookies.delete(key, options)
+				},
+			},
+		},
+	)
 
 	/**
 	 * a little helper that is written for convenience so that instead
@@ -20,11 +30,12 @@ export const handle: Handle = async ({event, resolve}) => {
 		const {
 			data: {session},
 		} = await event.locals.supabase.auth.getSession()
-
 		return session
 	}
 
 	return resolve(event, {
+		// headers will not be serialized, unless explicitly included here
+		// https://kit.svelte.dev/docs/hooks#server-hooks-handle
 		filterSerializedResponseHeaders(name) {
 			return name === 'content-range'
 		},
