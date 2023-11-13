c888338b3c8b81f79ac609aebb5231aa2e710c78
Johnny Magrippis <j@magrippis.com>
2023-11-13 07:22:40 +0100

    🐛  maybe it's the mutated mdParser which breaks things?

--- src/lib/server/mdParser.ts
+++ src/lib/server/mdParser.ts
@@ -2,19 +2,16 @@ import {getHighlighter} from 'shiki'
 import {Marked} from 'marked'
 import {markedSmartypants} from 'marked-smartypants'
 
-let mdParser: Marked
 export const getMdParser = async (): Promise<Marked> => {
-	if (!mdParser) {
-		const highlighter = await getHighlighter({
-			theme: 'dracula',
-		})
-		mdParser = new Marked(markedSmartypants(), {
-			renderer: {
-				code(code, lang = 'sh') {
-					return highlighter.codeToHtml(code, {lang})
-				},
+	const highlighter = await getHighlighter({
+		theme: 'dracula',
+	})
+
+	return new Marked(markedSmartypants(), {
+		renderer: {
+			code(code, lang = 'sh') {
+				return highlighter.codeToHtml(code, {lang})
 			},
-		})
-	}
-	return mdParser
+		},
+	})
 }
