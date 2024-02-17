4afb16f36d19a3a0254cfa5ade8fdb33fa5004ad
Johnny Magrippis <j@magrippis.com>
2024-02-17 12:49:36 +0100

    ⚡️  hopefully reduce bundle size by specifying shiki languages

--- src/lib/server/mdParser.ts
+++ src/lib/server/mdParser.ts
@@ -2,15 +2,32 @@ import {unified} from 'unified'
 import remarkParse from 'remark-parse'
 import remarkRehype from 'remark-rehype'
 import rehypeStringify from 'rehype-stringify'
-import rehypeShiki from '@shikijs/rehype'
+import rehypeShikiFromHighlighter from '@shikijs/rehype/core'
+import {getHighlighterCore} from 'shiki/core'
+import getWasm from 'shiki/wasm'
 
-export const parseMd = (md: string) =>
-	unified()
+export const parseMd = async (md: string) => {
+	const highlighter = await getHighlighterCore({
+		themes: [import('shiki/themes/dracula.mjs')],
+		langs: [
+			import('shiki/langs/css.mjs'),
+			import('shiki/langs/javascript.mjs'),
+			import('shiki/langs/shellscript.mjs'),
+			import('shiki/langs/svelte.mjs'),
+			import('shiki/langs/swift.mjs'),
+			import('shiki/langs/tsx.mjs'),
+			import('shiki/langs/typescript.mjs'),
+		],
+		loadWasm: getWasm,
+	})
+
+	return unified()
 		.use(remarkParse)
 		.use(remarkRehype)
-		.use(rehypeShiki, {
+		.use(rehypeShikiFromHighlighter, highlighter, {
 			theme: 'dracula',
 		})
 		.use(rehypeStringify)
 		.process(md)
 		.then(String)
+}
